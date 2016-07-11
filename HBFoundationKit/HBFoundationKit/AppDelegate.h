cfa944d733e0d15aeed9fd4fdc819e24f2f0047b
胡海兵 <huhaibing@gcn99.com>
2016-07-11 11:48:50 +0800

    新建
    新建工程

---
+++ HBFoundationKit/HBFoundationKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HBFoundationKit
+//
+//  Created by yik on 16/7/11.
+//  Copyright © 2016年 jzg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
